4f81bc97578ed09d6730d566ee808956dbe42197
Kyle <mcphylus@live.co.uk>
2021-06-24 19:32:42 +0100

    Added storage and state processing

--- backend/CSV.Api/Startup.cs
+++ backend/CSV.Api/Startup.cs
@@ -1,21 +1,20 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CSV.Api.Command;
+using CSV.Api.DB;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSV.Api
 {
     public class Startup
     {
+        private const string CorsOriginPolicy = "AllowSpecificOrigin";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,17 +25,32 @@ namespace CSV.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMediatR(typeof(ProcessCSVHandler));
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsOriginPolicy,
+                    builder => builder
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowAnyOrigin());
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CSV.Api", Version = "v1" });
             });
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            services.AddDbContext<CsvDbContext>(options =>
+                options.UseSqlServer("Server=localhost,1433;Database=csvData;User Id=sa;Password=yourStrong(!)Password"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CsvDbContext dbContext)
         {
+            dbContext.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -47,6 +61,8 @@ namespace CSV.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(CorsOriginPolicy);
 
             app.UseAuthorization();
 
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csv-front-end';
+
+  data:number[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  openFile(event: any) {
+    let input = event.target;
+    for (var index = 0; index < input.files.length; index++) {
+        let reader = new FileReader();
+        reader.onload = () => {
+            var text = reader.result as string;
+            this.data = text.split(',').map(entry => +entry);
+        }
+        reader.readAsText(input.files[index]);
+    };
+  }
+
+  save() {
+    this.http.post('https://localhost:5001/api/csv', this.data).subscribe(r => console.log(r));
+  }
 }
